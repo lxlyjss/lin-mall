ae7c5f144bb693952c4d2de69ee25a9eec7e0857
liuxinlin <liuxinlin@meishubao.com>
2020-12-10 14:02:58 +0800

    feat: 完善职位详情UI

--- src/components/position/lessonItem.vue
+++ src/components/position/lessonItem.vue
@@ -4,9 +4,11 @@
       <van-image src="" />
     </div>
     <div class="lesson-info">
-      <p class="lesson-title">案例打扫房间拉时间段发了多少放家里卡萨丁发了就拉开京东方</p>
+      <p class="lesson-title">
+        案例打扫房间拉时间段发了多少放家里卡萨丁发了就拉开京东方
+      </p>
       <p class="subtitle">111人次学习-8课时</p>
-      <p>
+      <p class="money">
         <span>￥34</span>
         <span>原价￥34</span>
       </p>
@@ -27,8 +29,8 @@ export default {
     const router = useRouter();
     const toLessonPage = () => {
       router.push({
-        path: "/company"
-      })
+        path: "/company",
+      });
     };
     return {
       toLessonPage,
@@ -37,24 +39,54 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.lesson-item{
-  padding: 20px;
+.lesson-item {
+  padding: 20px 5px;
   margin-bottom: 7px;
   background-color: $white;
-  .title {
-    display: flex;
-    justify-content: space-between;
-    .title-left {
-      font-weight: 900;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #efefef;
+  .lesson-left {
+    margin-right: 12px;
+    .van-image {
+      width: 86px;
+      height: 86px;
     }
-    .title-right {
-      color: $red;
-      font-weight: 600;
+  }
+  .lesson-info {
+    .lesson-title {
+      font-size: 14px;
+      font-weight: 400;
+      color: #333333;
+      line-height: 20px;
+    }
+    .subtitle {
+      font-size: 11px;
+      font-weight: 400;
+      color: #999999;
+      line-height: 20px;
     }
   }
   .subtitle {
     font-size: 14px;
     color: #999;
+    margin-top: 6px;
+  }
+  .money {
+    margin-top: 2px;
+    span:first-of-type {
+      font-size: 16px;
+      font-weight: bold;
+      color: #f21e2e;
+      margin-right: 5px;
+    }
+    span:last-of-type {
+      font-size: 10px;
+      font-weight: 400;
+      text-decoration: line-through;
+      color: #999999;
+      line-height: 20px;
+    }
   }
 }
 </style>
